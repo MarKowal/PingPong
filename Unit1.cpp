9f54049769dad15feeac3c3c285ce0e4fc7a4c4c
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2022-02-07 22:21:36 +0100

    Add ball and two paddles

--- Unit1.cpp
+++ Unit1.cpp
@@ -8,10 +8,56 @@
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TForm1 *Form1;
+
+int x = -8;
+int y = -8;
+
 //---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
         : TForm(Owner)
 {
 }
 //---------------------------------------------------------------------------
- 
+
+void __fastcall TForm1::TimerBallTimer(TObject *Sender)
+{
+        Ball->Left += x;
+        Ball->Top += y;
+
+        //odbicie od gornej krawedzi
+        if(Ball->Top <= Background->Top) y = -y;
+        //odbicie od dolnej krawedzi
+        if(Ball->Top >= Background->Height-Ball->Height) y = -y;
+
+        //przegrana czerwona
+        if(Ball->Left < RedPad->Left+RedPad->Width-130) {
+                TimerBall->Enabled = false;
+        }
+         //przegrana czarna
+        if(Ball->Left > BlackPad->Left+85) {
+                TimerBall->Enabled = false;
+        }
+
+}
+//---------------------------------------------------------------------------
+
+
+void __fastcall TForm1::RedUpTimer(TObject *Sender)
+{
+        RedPad->Top -= 10;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::RedDownTimer(TObject *Sender)
+{
+        RedPad->Top += 10;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::FormKeyDown(TObject *Sender, WORD &Key,
+      TShiftState Shift)
+{
+        //JAK ZROBI KLAIWSZ WIRTUALNY DLA A ORAZ Z???
+}
+//---------------------------------------------------------------------------
+
